b0787ebbc5442086f2cca9f0696ec6e877ecbc3b
dbring <dbring@gmail.com>
2023-01-07 15:01:56 -0800

    reduced max tokeens

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ const Home = () => {
 
     try {
       console.log("Calling OpenAI...");
-      const response = await fetch("/api/generate", {
+      const response = await fetch("./api/generate", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
